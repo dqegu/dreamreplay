1af8f3100dbf52b23ccc158e3d8b2d0d3a9d3ad3
A.Obure <ao1g22@loginX001.cluster.local>
2026-04-22 13:31:42 +0100

    Transfer outputs to remote

--- launch.sh
+++ launch.sh
@@ -15,6 +15,8 @@ conda activate spens-seq
 export OMP_NUM_THREADS=8
 export IRP_BASE_DIR=/iridisfs/home/ao1g22/comp6228/irp
 
+export LD_LIBRARY_PATH=/home/ao1g22/.conda/envs/spens-seq/lib/python3.11/site-packages/nvidia/cublas/lib:/home/ao1g22/.conda/envs/spens-seq/lib/python3.11/site-packages/nvidia/cuda_runtime/lib:/home/ao1g22/.conda/envs/spens-seq/lib/python3.11/site-packages/nvidia/cudnn/lib:/home/ao1g22/.conda/envs/spens-seq/lib/python3.11/site-packages/nvidia/cufft/lib:/home/ao1g22/.conda/envs/spens-seq/lib/python3.11/site-packages/nvidia/curand/lib:/home/ao1g22/.conda/envs/spens-seq/lib/python3.11/site-packages/nvidia/cusolver/lib:/home/ao1g22/.conda/envs/spens-seq/lib/python3.11/site-packages/nvidia/cusparse/lib:/home/ao1g22/.conda/envs/spens-seq/lib/python3.11/site-packages/nvidia/nccl/lib:$LD_LIBRARY_PATH
+
 cd /iridisfs/home/ao1g22/comp6228/irp
 
 python run_all.py --seeds 0 7 42 --n_train 4000 --n_test 1000
